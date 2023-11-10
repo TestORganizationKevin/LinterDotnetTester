eded242477b19b13b0c5014d9534ad150e907651
Kevin <kevinb@softcontrol.dk>
2023-11-10 11:06:42 +0100

    sss

--- Program.cs
+++ Program.cs
@@ -9,7 +9,23 @@
 
     public static void Nobody()
     {
-        
+        byte vbyte = new(); //byte  
+        sbyte vsbyte = new(); //sbyte  
+        short vshort = new(); //short  
+        ushort vushort = new(); //ushort  
+        int vint = new(); //int  
+        uint vuint = new(); //uint  
+        long vlong = new(); //long  
+        ulong vulong = new(); //ulong  
+        float vfloat = new(); //float  
+        double vdouble = new(); //double  
+        decimal vdecimal = new(); //decimal  
+        char vchar = new(); //char  
+        bool vbool = new(); //bool  
+        object vobject = new(); //object  
+        string string1 = new("sda"); //string   
+        string string2 = new("sda"); //string   
+        DateTime vDateTime = new(); //DateTime  
     }
     void adnadhgadgshjkgdhjksgdhhgsdkha(){}
 }
