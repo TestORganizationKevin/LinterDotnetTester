8f892f2ed9c2896a423909e69c08d43ddca72cd6
Kevin <kevinb@softcontrol.dk>
2023-11-13 08:13:36 +0100

    ...

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
-﻿class Program
+﻿using System.Text.RegularExpressions;
+
+class Program
 {
     static void Main(string[] args)
     {
-        GoodFunctionName();
-        badFunctionName();
+        Console.Clear();
+        string input =
+            @"public static void GoodFunctionName() \n {\n        byte vbyte = new(); //byte  \n        sbyte vsbyte = new(); //sbyte  \n        short vshort = new(); //short  \n        ushort vushort = new(); //ushort  \n        int vint = new(); //int  \n        uint vuint = new(); //uint  \n        long vlong = new(); //long  \n        ulong vulong = new(); //ulong  \n        float vfloat = new(); //float  \n        double vdouble = new(); //double  \n        decimal vdecimal = new(); //decimal  \n        char vchar = new(); //char  \n        bool vbool = new(); //bool  \n        // object a = new(); //object  \n        string string1 = new(""sda""); //string   \n        string string2 = new(""sda""); //string   \n        DateTime vDateTime = new(); //DateTime  \n    }\n\n    static void badFunctionName()\n    {\n    }";
+        string pattern = @"";
+
+        // Regex regex = new Regex(@"\b([a-z]\w*(?=\())");
+
+        Console.WriteLine("\n");
+
+        foreach (Match match in Regex.Matches(input, pattern))
+        {
+            Console.WriteLine($"Matched text: {match.Value}");
+            // Console.WriteLine(doesmatch.Success);
+        }
     }
 
     public static void GoodFunctionName()
@@ -29,6 +43,5 @@
 
     static void badFunctionName()
     {
-        
     }
 }
