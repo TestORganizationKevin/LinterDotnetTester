5e7a4ca825df37aadf7a1061e5ca1eaba207c340
Kevin <kevinb@softcontrol.dk>
2023-11-13 08:55:41 +0100

    removed that ugly pattern

--- Program.cs
+++ Program.cs
@@ -7,10 +7,7 @@ class Program
         Console.Clear();
         string input =
             @"public static void GoodFunctionName() \n {\n        byte vbyte = new(); //byte  \n        sbyte vsbyte = new(); //sbyte  \n        short vshort = new(); //short  \n        ushort vushort = new(); //ushort  \n        int vint = new(); //int  \n        uint vuint = new(); //uint  \n        long vlong = new(); //long  \n        ulong vulong = new(); //ulong  \n        float vfloat = new(); //float  \n        double vdouble = new(); //double  \n        decimal vdecimal = new(); //decimal  \n        char vchar = new(); //char  \n        bool vbool = new(); //bool  \n        // object a = new(); //object  \n        string string1 = new(""sda""); //string   \n        string string2 = new(""sda""); //string   \n        DateTime vDateTime = new(); //DateTime  \n    }\n\n    static void badFunctionName()\n    {\n    }";
-        string pattern = @"";
-
-        // Regex regex = new Regex(@"\b([a-z]\w*(?=\())");
-
+        string pattern = @"\b((?!new\s*\()[a-z]\w*(?=\())";
         Console.WriteLine("\n");
 
         foreach (Match match in Regex.Matches(input, pattern))
